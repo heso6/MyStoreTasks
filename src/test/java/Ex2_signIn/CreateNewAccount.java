540dd8ee4b4ead212b8a5ae2d1174a91e131d75a
heso6 <romaniuk99@gmail.com>
2022-05-19 10:17:51 +0200

    Next Commit

--- src/test/java/Ex2_signIn/CreateNewAccount.java
+++ src/test/java/Ex2_signIn/CreateNewAccount.java
@@ -1,9 +1,14 @@
 package Ex2_signIn;
 
 import ConfigurationBase.ConfigurationBase;
+import net.bytebuddy.utility.RandomString;
+import org.apache.commons.lang3.RandomStringUtils;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import java.util.Random;
 
 public class CreateNewAccount extends ConfigurationBase {
     @Test
@@ -21,5 +26,28 @@ public class CreateNewAccount extends ConfigurationBase {
 
         WebElement lastName = webDriver.findElement(By.cssSelector("[name='lastname']"));
         lastName.sendKeys("Kowalski");
+
+        WebElement email = webDriver.findElement(By.cssSelector(".form-control[name='email']"));
+        email.sendKeys(generateRandomText()+"@gmail.com");
+
+        WebElement password = webDriver.findElement(By.cssSelector("[name='password']"));
+        password.sendKeys("qwerty!");
+
+        WebElement customerPrivacyCheckBox = webDriver.findElement(By.cssSelector("[name='customer_privacy']"));
+        customerPrivacyCheckBox.click();
+
+//        psgdpr checkBox
+        webDriver.findElement(By.cssSelector("[name='psgdpr']")).click();
+
+//        submitButton
+        webDriver.findElement(By.cssSelector("[data-link-action='save-customer']")).click();
+
+        String getAccountNameAndLastName = webDriver.findElement(By.cssSelector(".account .hidden-sm-down")).getText();
+        Assert.assertEquals(getAccountNameAndLastName,"Bożydar" + " " + "Kowalski");
+
+    }
+
+    public String generateRandomText(){
+        return RandomStringUtils.randomAlphabetic(6);
     }
 }
